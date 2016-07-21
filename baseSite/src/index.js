49eb0befd278ecab2342153c361e9afee97f5e0b
Chris Richards <ripter001@gmail.com>
2016-07-20 23:11:09 -0700

    golfed the slot code, added createSlot method.

--- baseSite/src/index.js
+++ baseSite/src/index.js
@@ -1,13 +1,22 @@
-const app = require('./app.jsx');
-
-// window.onDivLoad = function() {
-//   debugger;
-// }
-
 window.TOUT = {
+  // Called when a slot has loaded on the page and waiting for our response.
   loadSlot(elm, intervalID) {
+    // Cancel the call home.
     clearInterval(intervalID);
 
+    // Do whatever we want here.
     console.log('loaded slot', elm);
+  },
+
+  // This will create a slot with id
+  createSlot(id) {
+    var slotCode = '!function(a){var b=setInterval(function(){window.TOUT&&TOUT.loadSlot(a,b)},100)}("'+id+'");'
+    var div = document.createElement('div');
+    var script = document.createElement('script');
+
+    div.id = id;
+    script.innerHTML = slotCode;
+    div.appendChild(script);
+    return div;
   }
 };
