2514f89454a1b58b90a62316e26c62e2452de667
Chris Richards <ripter001@gmail.com>
2017-03-18 17:26:43 -0700

    clicking works properly

--- lightsOut/src/index.js
+++ lightsOut/src/index.js
@@ -27,6 +27,34 @@ const state = {
     const x = index - (y * width)
     return {x, y};
   },
+
+  // Toggle the grid lights starting at point
+  // This toggles the lights in a cross pattern
+  togglePoint(x, y) {
+    const { board, width, height } = this;
+    const toggle = (x, y) => {
+      board[x][y] = board[x][y] === 0 ? 1 : 0;
+    };
+
+    if (y-1 >= 0) {
+      toggle(x, y-1);
+    }
+    if (x-1 >= 0) {
+      toggle(x-1, y);
+    }
+    if (x >= 0) {
+      toggle(x, y);
+    }
+    if (x+1 < width) {
+      toggle(x+1, y);
+    }
+    if (y+1 < height) {
+      toggle(x, y+1);
+    }
+
+    // trigger render
+    lens.render(state);
+  }
 };
 
 
@@ -52,32 +80,18 @@ const lens = new LensDOM({
       return result;
     },
 
-    // adds event listener
+    // on cell click
     onclick(evt, elm, index) {
-      //TODO: Turn into Action Light Pressed
       const { board } = this; // state === this
       const { x, y } = this.getPoint(index);
-      let val = board[x][y];
-      console.log('click', evt);
 
-      // toggle val
-      if (val === 0) { val = 1; }
-      else { val = 0; }
-
-      // update the state
-      board[x][y] = val;
-      // trigger render
-      lens.render(state);
+      // Action: toggle point
+      this.togglePoint(x, y);
     },
-  },
-  // selectors that don't match are skipped
-  'invalid': {
-    test: true,
   },
 });
 
 // Render the inital application state
-// updateUI(state, lensUI);
 lens.render(state);
 
 // debugging fun
