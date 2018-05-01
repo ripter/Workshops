ac98aeeeab0a3668ab2d08199f7cd80e70ed742f
Chris Richards <ripter001@gmail.com>
2018-04-30 22:24:44 -0700

    i forgot

--- unity/kennyTest/Assets/src/Scripts/MoveAgentToMousePosition.cs
+++ unity/kennyTest/Assets/src/Scripts/MoveAgentToMousePosition.cs
@@ -3,23 +3,35 @@ using UnityEngine.AI;
 using UnityStandardAssets.Characters.ThirdPerson;
 
 // Moves the player using a NavMesh and NavAgent
-public class MoveAgentToMousePosition : MonoBehaviour {
+public class MoveAgentToMousePosition : MonoBehaviour
+{
 	public ThirdPersonCharacter character;
 	NavMeshAgent agent;
 
-	void Start () {
+	void Start ()
+	{
 		// Get the NavMeshAgent component instance that is on our gameObject.
 		agent = GetComponent<NavMeshAgent> ();
 		agent.updateRotation = false; // Let the animation control rotation.
 	}
 
-	void Update() {
+	void Update ()
+	{
 		// Fire1 is mouse left click.
-		if (Input.GetButton("Fire1")) {
+		if (Input.GetButton ("Fire1")) {
 			RaycastHit hit;
 
-			if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
-				agent.destination = hit.point;
+			if (Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hit, 100)) {
+				// did the user click the ground or a clickable object?
+				if (hit.collider.isTrigger) {
+					Clickable clickable = hit.collider.GetComponent<Clickable> ();
+					Debug.Log ("Clickable Dest: " + clickable.name);
+					agent.destination = clickable.destination.position;
+				} else {
+					Debug.Log ("Floor Click");
+					agent.destination = hit.point;
+				}
+
 			}
 		}
 
