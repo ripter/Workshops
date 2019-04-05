04066942d7727471698c48769ce97d56c93ebb28
Chris Richards <ripter001@gmail.com>
2019-04-04 22:12:42 -0700

    Basics of rendering and some click handling for lights out

---
+++ ReactJS/lights-out/src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const GRID_WIDTH = 5;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      grid: [
+        0,0,0,0,0,
+        0,0,1,0,0,
+        0,1,1,1,0,
+        0,0,1,0,0,
+        0,0,0,0,0,
+      ],
+    };
+  }
+  toggleGridCell(index) {
+    const { grid } = this.state;
+    const x = 0| index % GRID_WIDTH;
+    const y = 0| index / GRID_WIDTH;
+    const atPos = (x, y) => x + (y * GRID_WIDTH);
+
+    // toggle in a cross pattern
+    // NOTE: this converts our 1/0s into true/false values, which is ok.
+    grid[atPos(x, y)] = !grid[atPos(x, y)];
+    grid[atPos(x, y+1)] = !grid[atPos(x, y+1)];
+    grid[atPos(x, y-1)] = !grid[atPos(x, y-1)];
+    grid[atPos(x+1, y)] = !grid[atPos(x+1, y)];
+    grid[atPos(x-1, y)] = !grid[atPos(x-1, y)];
+
+    this.setState({grid});
+  }
+  render() {
+    const { grid } = this.state;
+    return (
+      <div>
+        <div className="grid">{grid.map((isOn, index) => {
+          return (
+            <div className="grid-cell" onClick={() => this.toggleGridCell(index)} key={index}>{isOn ? 1 : 0}</div>
+          );
+        })}</div>
+      </div>
+    );
+  }
+}
+
+export default App;
