97c62ad6ec3b48804f2a7c66846a8741ffb0ad69
Chris Richards <ripter001@gmail.com>
2019-06-15 10:56:10 -0700

    basic phong shader works

--- aframe/src/shader-phong.js
+++ aframe/src/shader-phong.js
@@ -2,29 +2,23 @@
 AFRAME.registerShader('phong', {
   schema: {
     color: {default: '#fff'},
-    emissive: {default: '#fff'},
-    wireframe: {default: true},
+    wireframe: {default: false},
     src: {type: 'map'},
   },
 
   init(data) {
     const system = this.el.sceneEl.systems.material;
-    console.group('phong shader');
-    console.log('system.material', system);
-    console.table('data', data);
-
+    // Load the src as a Texture and apply it to the material
     system.loadImage(data.src, data, (texture) => {
-      console.log('image loaded', arguments);
-      console.log('texture', texture);
       // Update the material with the loaded texture
       this.material.map = texture;
       this.material.needsUpdate = true;
     });
 
-    // Create the inital material
+    // initial material
     this.material = new THREE.MeshPhongMaterial({
       color: data.color,
+      wireframe: data.wireframe,
     });
-    console.groupEnd();
   },
 });
