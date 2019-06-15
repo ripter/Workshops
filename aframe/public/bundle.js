35a96e75ceaac245aa07bfb1011818ce4b462ee4
Chris Richards <ripter001@gmail.com>
2019-06-15 10:20:40 -0700

    Adding phong shader

--- aframe/public/bundle.js
+++ aframe/public/bundle.js
@@ -59,6 +59,36 @@
 
   });
 
+  AFRAME.registerShader('phong', {
+    schema: {
+      color: {default: '#fff'},
+      emissive: {default: '#fff'},
+      wireframe: {default: true},
+      src: {type: 'map'},
+    },
+
+    init(data) {
+      const system = this.el.sceneEl.systems.material;
+      console.group('phong shader');
+      console.log('system.material', system);
+      console.table('data', data);
+
+      system.loadImage(data.src, data, (texture) => {
+        console.log('image loaded', arguments);
+        console.log('texture', texture);
+        // Update the material with the loaded texture
+        this.material.map = texture;
+        this.material.needsUpdate = true;
+      });
+
+      // Create the inital material
+      this.material = new THREE.MeshPhongMaterial({
+        color: data.color,
+      });
+      console.groupEnd();
+    },
+  });
+
   const _loadAt = (new Date()).toISOString();
 
   console.log('bundle.js loaded at', _loadAt);
--- aframe/src/index.js
+++ aframe/src/index.js
@@ -1,4 +1,5 @@
 const _loadAt = (new Date()).toISOString();
 import './material-cube.js';
+import './shader-phong.js';
 
 console.log('bundle.js loaded at', _loadAt);
