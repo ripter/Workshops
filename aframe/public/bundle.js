f70d9d5a55d74375cc7327fcdbba74e1a0b148ac
Chris Richards <ripter001@gmail.com>
2019-06-15 20:23:41 -0700

    Movement via go controler!

--- aframe/public/bundle.js
+++ aframe/public/bundle.js
@@ -91,25 +91,38 @@
     // },
 
     init() {
-      const elLog = document.querySelector('#logDebug2');
+      const { el } = this;
 
-      this.el.addEventListener('trackpadchanged', (event) => {
-        console.log('trackpadchanged', event);
-        // elLog.setAttribute('value', `tpChd`);
-        logIt(event);
-      });
-      this.el.addEventListener('triggerchanged', (event) => {
-        console.log('triggerchanged', event);
-        elLog.setAttribute('value', `triggerchanged`);
-      });
+      el.addEventListener('trackpadup', this.handleTrackPadClick.bind(this));
+      // const elLog = document.querySelector('#logDebug2');
+      // ['trackpadtouchstart', 'trackpaddown', 'triggerchanged', 'buttonchanged']
+      // 'trackpadchanged',
+      // ['trackpadtouchstart', 'trackpaddown', 'triggerchanged',
+      //  'buttonchanged'].forEach((eventName) => {
+      //   this.el.addEventListener(eventName, logIt);
+      // });
+      // this.el.addEventListener('trackpaddown', this.handleTrackPadClick);
+      // this.el.addEventListener('axismove', (event) => {
+      //   const elLog = document.querySelector('#logDebug2');
+      //   const axis = event.detail.axis.map(num => (0|num*100)/100);
+      //   const msg = `Axis: ${axis} ~ ${this.el.components['tracked-controls'].axis}`;
+      //   elLog.setAttribute('value', msg);
+      // });
+
     },
 
-  });
+    handleTrackPadClick() {
+      const elLog = document.querySelector('#logDebug2');
+      const axis = this.el.components['tracked-controls'].axis.map(num => (0|num*100)/100);
+      const msg = `Axis: ${axis}`;
+      elLog.setAttribute('value', msg);
 
-  function logIt(event) {
-    const elLog = document.querySelector('#logDebug2');
-    elLog.setAttribute('value', `o: ${event.detail.value}`);
-  }
+      const player = document.querySelector('#player').object3D;
+      const delta = new THREE.Vector3(axis[0], 0, axis[1]);
+      player.position.add(delta);
+    }
+
+  });
 
   const _loadAt = (new Date()).toISOString();
 
