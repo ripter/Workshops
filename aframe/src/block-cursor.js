97312305f84afc17fd0751f4562ec84b5036d52b
Chris Richards <ripter001@gmail.com>
2019-06-19 11:50:10 -0700

    Cursor on correct face

--- aframe/src/block-cursor.js
+++ aframe/src/block-cursor.js
@@ -9,6 +9,7 @@ AFRAME.registerComponent('block-cursor', {
   init() {
     // console.log('init block-cursor', this.data, this);
     this.elCursor = this.initCursor();
+    this.cursor = this.elCursor.object3D;
 
     // this.el.addEventListener('raycaster-intersection', (evt) => {
     //   console.log('intersection', evt);
@@ -21,21 +22,31 @@ AFRAME.registerComponent('block-cursor', {
   },
 
   tick() {
+    const { cursor } = this;
     const { raycaster } = this.el.components;
     if (!raycaster) { return; }
     const { intersections } = raycaster;
     if (!intersections || intersections.length === 0) {
+      cursor.visible = false;
       logCamera('');
       return;
     }
     // console.log('intersection', intersections[0]);
-    const { distance } = intersections[0];
+    const { distance, point } = intersections[0];
+    console.log('intersection', intersections[0]);
     logCamera(`dist: ${fmtNumber(distance)}`);
+
+    cursor.visible = true;
+    cursor.position.set(point.x, point.y, point.z);
   },
 
   initCursor() {
-    const elm = document.createElement('a-entity');
+    const elm = document.createElement('a-box');
     elm.id = 'block-cursor--cursor';
+    elm.setAttribute('color', 'orange');
+    elm.setAttribute('depth', 0.5);
+    elm.setAttribute('width', 0.5);
+    elm.setAttribute('height', 0.5);
     this.el.sceneEl.append(elm)
     return elm;
   },
