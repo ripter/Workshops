0209e98e51744f49f8c05b9fefff2553595f8bf6
Chris Richards <ripter001@gmail.com>
2019-07-18 22:29:43 -0700

    WIP: grabbing sorta works

--- aframe/public/bundle.js
+++ aframe/public/bundle.js
@@ -201,7 +201,7 @@
     },
 
     handleEvent(event) {
-      console.log('player-hand.handleEvent', event.type, event, this);
+      // console.log('player-hand.handleEvent', event.type, event, this);
     },
 
     remove() {
@@ -255,16 +255,42 @@
     // },
 
     init() {
-      this.el.addEventListener('collidestart', this.onCollideStart.bind(this));
-      this.el.addEventListener('collideend', this.onCollideEnd.bind(this));
+      // Use physics collision events.
+      // this.el.addEventListener('collidestart', this)
+      // this.el.addEventListener('collideend', this)
+
+      // Use Interaction system events.
+      this.el.addEventListener('handenter', this);
+      this.el.addEventListener('handleave', this);
     },
 
-    onCollideStart(event) {
-      console.log('grab-able collidestart', event);
+    handleEvent(event) {
+      // console.log('hover-able handleEvent', event.type, event, this);
+      switch (event.type) {
+        case 'collidestart':
+        case 'handenter':
+          const { hand } = event.detail;
+          console.log('handenter starting grabStart', event);
+          this.grabStart(hand);
+          // AFRAME.utils.entity.setComponentProperty(this.el, 'material.opacity', 0.5);
+          break;
+        case 'collideend':
+        case 'handleave':
+          AFRAME.utils.entity.setComponentProperty(this.el, 'material.opacity', 1.0);
+          break;
+        default:
+          throw new Error(`hover-able does not have a case for event "${event.type}"`);
+      }
     },
 
-    onCollideEnd(event) {
-      console.log('grab-able collideend', event);
+    grabStart(hand) {
+      const isHandGripped = AFRAME.utils.entity.getComponentProperty(hand, 'player-hand.isGrip');
+      console.log('isHandGripped', isHandGripped);
+      if (!isHandGripped) { return; }
+
+      console.log('GRIP!', hand);
+      // Bind to the hand.
+      hand.setAttribute('ammo-constraint', 'target: #gripMe;');
     },
   });
 
