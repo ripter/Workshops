bed465bdb341e879234f8fa0a0fa15ac87c0ef10
Chris Richards <ripter001@gmail.com>
2019-11-23 21:15:19 -0800

    Empty base project in characters folder

---
+++ aframe/characters/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: false,
+    port: 9000
+  }
+};
