b152246b495a3900b32c90c78dd63b7297f80236
Chris Richards <ripter001@gmail.com>
2019-11-30 16:11:08 -0800

    Playing baked animation

---
+++ aframe/characters/src/animation-control.js
@@ -0,0 +1,93 @@
+// gltf-animations
+
+AFRAME.registerComponent('animation-control', {
+  schema: {
+    actionName: {default: 'Idle'},
+  },
+
+  /**
+   * Init handler. Similar to attachedCallback.
+   * Called during component initialization and is only run once.
+   * Components can use this to set initial state.
+   */
+  init() {
+    const { el } = this;
+
+    this.mixer = null; // https://threejs.org/docs/index.html#api/en/animation/AnimationMixer
+    this.action = null; // https://threejs.org/docs/index.html#api/en/animation/AnimationAction
+
+    // listen to changes on the refrence objects like 'mesh'
+    el.addEventListener('object3dset', this);
+  },
+
+  /**
+   * Update handler. Similar to attributeChangedCallback.
+   * Called whenever component's data changes.
+   * Also called on component initialization when the component receives initial data.
+   *
+   * @param {object} prevData - Previous attributes of the component.
+   */
+  update(oldData) {
+    console.log('animation-control.update', this.data);
+  },
+
+  /**
+   * Tick handler.
+   * Called on each tick of the scene render loop.
+   * Affected by play and pause.
+   *
+   * @param {number} time - Scene tick time.
+   * @param {number} timeDelta - Difference in current render time and previous render time.
+   */
+  tick (time, timeDelta) {
+    const deltaInSeconds = timeDelta / 1000;
+
+    if (this.mixer) {
+      this.mixer.update(deltaInSeconds);
+    }
+  },
+
+  /**
+   * Remove handler. Similar to detachedCallback.
+   * Called whenever component is removed from the entity (i.e., removeAttribute).
+   * Components can use this to reset behavior on the entity.
+   */
+  remove() {
+    delete this.mixer;
+    delete this.action;
+  },
+
+
+  /**
+   * Called when a listening event is observed.
+   * @param  {Event} event An Event object describing the event that has been fired and needs to be processed.
+   * @return {undefined}
+   */
+  handleEvent(event) {
+    switch (event.type) {
+      case 'object3dset':
+        // only respond to 'mesh' changes
+        if (event.detail.type !== 'mesh') { return; }
+        // update mixer with the new mesh
+        return this.updateMixer(event.detail.object);
+      default:
+        console.warn(`Unhandled event type: ${event.type}`, event);
+    }
+  },
+
+
+  // Update the Mixer with a new Root Object
+  updateMixer(rootObj) {
+    const { animations } = rootObj;
+    const { actionName } = this.data;
+
+    // Update the mixer to use the new root object.
+    this.mixer = new THREE.AnimationMixer(rootObj);
+
+    // get and play the named action
+    const clip = THREE.AnimationClip.findByName( animations, actionName );
+    const action = this.action = this.mixer.clipAction( clip );
+    action.play();
+    console.log('action', action);
+  },
+});
--- aframe/characters/src/index.js
+++ aframe/characters/src/index.js
@@ -1,5 +1,6 @@
 import './gltf-model-2';
 import './material-2';
 import './gltf-animations';
+import './animation-control';
 
 console.log('Everything Loaded and Ready!');
