586b80d571309d8981b29e1e5cdbec1f452320f7
Chris Richards <chrisrichards@Chriss-MacBook-Pro-2.local>
2020-01-18 10:04:01 -0800

    Init new A-Frame project

---
+++ aframe/animatedWalkLoop/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    THREE: 'readonly',
+    AFRAME: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'import/prefer-default-export': 'off',
+    'no-void': 'off',
+    'no-param-reassign': 'off',
+    'no-multi-assign': 'off',
+  },
+};
