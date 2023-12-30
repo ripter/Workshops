f5dfb0f2d05a22843f5a9be5a32d25e164d57426
Chris <ripter001@gmail.com>
2023-12-30 09:59:50 -0500

    got redirect working

--- nim/RockPaperScissorsSpockLizard/src/main.nim
+++ nim/RockPaperScissorsSpockLizard/src/main.nim
@@ -2,8 +2,19 @@ import happyx
 
 # Serve app at http://localhost:5000
 serve "127.0.0.1", 5000:
+  staticDir "public"
+
   # GET Method
-  get "/":
+  get "/api/hello":
     # Respond plaintext
     return "Hello, world"
 
+  get "/":
+    answerHtml req, """
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta http-equiv="Refresh" content="0; URL=/public/index.html" />
+    </head>
+  </html>
+    """
