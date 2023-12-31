81ebe0514034cd25afcc0531ad2e8624e8497ae1
Chris <ripter001@gmail.com>
2023-12-30 21:59:02 -0500

    created the missing answerRedirect template

--- nim/RockPaperScissorsSpockLizard/src/main.nim
+++ nim/RockPaperScissorsSpockLizard/src/main.nim
@@ -1,46 +1,28 @@
 import happyx
+import ./ssrutils
 
 const PORT = 5000
 const HOST = "127.0.0.1"
 const ROOT = "public"
 
+
+
 echo &"Starting server on http://{HOST}:{PORT}/"
-
-model Echo:
-  message: string
-
 serve HOST, PORT:
   staticDir "public"
 
   # Everyone needs a Hello World API.
   get "/api/helloWorld":
-    answerJson req, { 
-      "message": "Hello, world" 
+    return %*{ 
+      "message": "Hello, world",
     }
   
   # Echo is fun for friends and family.
   get "/api/echo/{message:string}":
-    echo &"Echoing {message}"
-    answerJson req, {
+    return %*{
       "echo": message,
     }
 
-
-  get "/moved":
-    answer req, &"", Http301, newHttpHeaders(
-      [
-        ("Content-Type", "text/plain; charset=utf-8"), 
-        ("Location", "/")
-      ]
-    ) 
-
   # Redirect to index.html in the public directory
   get "/":
-    answerHtml req, &"""
-  <!DOCTYPE html>
-  <html>
-    <head>
-      <meta http-equiv="Refresh" content="0; URL=/{ROOT}/index.html" />
-    </head>
-  </html>
-    """
+    answerRedirect req, &"/{ROOT}/index.html"
