496b02454eecaefe70b242af1ce43eefaa11d1a9
ripter <ripter001@gmail.com>
2024-06-29 16:01:33 -0400

    WIP: Click to change tile

--- c/raylib-tic-tac-toe/main.c
+++ c/raylib-tic-tac-toe/main.c
@@ -23,6 +23,9 @@ int main(void)
     PLAYER_O, EMPTY, EMPTY,
   };
 
+  // Buffer to hold on-screen text
+  char buffer[256] = {0};
+
 
   // Initialize window and the OpenGL context
   InitWindow(config.screenWidth, config.screenHeight, config.windowTitle);
@@ -48,9 +51,9 @@ int main(void)
   while (!WindowShouldClose()) // Detect window close button or ESC key
   {
     // Update State
-
     if (IsKeyPressed(KEY_ONE)) {
       camera.zoom = 1.0f;
+      // SetMouseScale(1.0f, 1.0f);
     }
     else if (IsKeyPressed(KEY_TWO)) {
       camera.zoom = 2.0f;
@@ -72,6 +75,7 @@ int main(void)
     }
     else if (IsKeyPressed(KEY_EIGHT)) {
       camera.zoom = 8.0f;
+      // SetMouseScale(8.0f, 8.0f);
     }
     else if (IsKeyPressed(KEY_NINE)) {
       camera.zoom = 9.0f;
@@ -81,6 +85,21 @@ int main(void)
     }
 
 
+    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+      Vector2 mousePos = GetMousePosition();
+      int x = (int)(mousePos.x / (config.tileSize + GRID_PADDING));
+      int y = (int)(mousePos.y / (config.tileSize + GRID_PADDING));
+      int idx = x + (y * 3);
+      if (gameBoard[idx] == EMPTY) {
+        gameBoard[idx] = PLAYER_X;
+      }
+      else if (gameBoard[idx] == PLAYER_X) {
+        gameBoard[idx] = PLAYER_O;
+      }
+      else if (gameBoard[idx] == PLAYER_O) {
+        gameBoard[idx] = EMPTY;
+      }
+    }
 
 
     // Draw State
@@ -90,6 +109,9 @@ int main(void)
       BeginMode2D(camera);
         drawGameBoard(texturePacked, gameBoard, config.tileSize, GRID_PADDING, framePlayerX, framePlayerY);
       EndMode2D();
+
+      snprintf(buffer, sizeof(buffer), "Camera Zoom: %.0f", camera.zoom);
+      DrawText(buffer, 0, 0, 8, GRAY);
     EndDrawing();
   }
 
